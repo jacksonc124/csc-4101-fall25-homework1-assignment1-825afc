7d9ad63ecc9a83ed29df273046918ceb9375eaeb
jacksonc124 <jcogan2@lsu.edu>
2025-09-07 18:10:04 -0500

    hw 1 - rubyBasics

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,35 @@
 class PhoneBook
-    def initialize
+  def initialize
+    @entries = {}
+  end
+
+  def add(name, number, is_listed)
+    # if name already exists
+    return false if @entries.key?(name)
+
+    return false unless number.match?(/^\d{3}-\d{3}-\d{4}$/)
+
+    # number can be listed only once
+    if is_listed
+      return false if @entries.values.any? { |e| e[:number] == number && e[:is_listed] }
     end
 
-    def add(name, number, is_listed)
-        raise Exception, "Not implemented"
-    end
+    @entries[name] = { number: number, is_listed: is_listed }
+    true
+  end
 
-    def lookup(name)
-        raise Exception, "Not implemented"
-    end
+  def lookup(name)
+    entry = @entries[name]
+    return nil if entry.nil?
+    entry[:is_listed] ? entry[:number] : nil
+  end
 
-    def lookupByNum(number)
-        raise Exception, "Not implemented"
-    end
+  def lookupByNum(number)
+    entry = @entries.find { |_, v| v[:number] == number && v[:is_listed] }
+    entry ? entry[0] : nil
+  end
 
-    def namesByAc(areacode)
-        raise Exception, "Not implemented"
-    end
+  def namesByAc(areacode)
+    @entries.select { |_, v| v[:number].start_with?(areacode) }.keys
+  end
 end
--- src/warmup.rb
+++ src/warmup.rb
@@ -1,23 +1,36 @@
 def fib(n)
-    raise Exception, "Not Implemented"
+  return [] if n == 0
+  return [0] if n == 1
+
+  seq = [0, 1]
+  while seq.length < n
+    seq << seq[-1] + seq[-2]
+  end
+  seq
 end
 
 def isPalindrome(n)
-    raise Exception, "Not Implemented"
+  s = n.to_s
+  s == s.reverse
 end
 
 def nthmax(n, a)
-    raise Exception, "Not Implemented"
+  uniq_sorted = a.uniq.sort.reverse
+  uniq_sorted[n]
 end
 
 def freq(s)
-    raise Exception, "Not Implemented"
+  return "" if s.empty?
+  counts = Hash.new(0)
+  s.each_char { |c| counts[c] += 1 }
+  counts.max_by { |_, v| v }[0]
 end
 
 def zipHash(arr1, arr2)
-    raise Exception, "Not Implemented"
+  return nil unless arr1.length == arr2.length
+  Hash[arr1.zip(arr2)]
 end
 
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+  hash.map { |k, v| [k, v] }
 end
